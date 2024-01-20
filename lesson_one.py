a46aa91d89b32dd27a01326e23a6e620758c31bc
wangzhen <274199091@qq.com>
2024-01-20 20:31:30 +0800

    提交代码

---
+++ lesson_one.py
@@ -0,0 +1,11 @@
+# Time：2024/1/20 7:54 PM wangzhen np_pd_matplot
+# Author : WangZhen
+import numpy
+import matplotlib.pyplot as plt
+
+x = list(range(1, 5))
+y = [2, 3, 4, 5]
+plt.figure(figsize=(10, 5),dpi=100)
+
+plt.plot(x, y)
+plt.show()
